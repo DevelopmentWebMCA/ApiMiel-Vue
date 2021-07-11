289f23c9ee242883e3050df1f5280e43b71267db
leslie luna <leslie.lunarms@gmail.com>
2021-07-10 19:55:47 -0500

    modulo de usuarios con formularios

--- src/views/MODULO_USUARIOS/AgregarUsuario.vue
+++ src/views/MODULO_USUARIOS/AgregarUsuario.vue
@@ -1,12 +1,174 @@
 <template>
+  <div>
+    <div
+      class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center profile-header"
+      style="
+        min-height: 600px;
+        background-size: cover;
+        background-position: center top;
+      "
+    >
+      <b-container fluid>
+        <b-row>
+          <b-col>
+            <h1>Usuarios</h1>
+          </b-col>
+        </b-row>
 
+        <b-jumbotron
+          bg-variant="white"
+          class="shadow-lg p-5 mb-5 bg-white rounded"
+        >
+          <h2 class="text-center fontLabel">Agregar usuario</h2>
+          <hr class="separador mt-2"/>
+          <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-container>
+              <b-row>
+                <b-col>
+                  <b-form-group
+                    id="input-group-name"
+                    label="Nombre Completo:"
+                    label-for="input-name"
+                    class="fontLabel"
+                  >
+                    <b-form-input
+                      id="input-name"
+                      v-model="form.name"
+                      placeholder="Escribir nombre completo"
+                      required
+                      class="fontInput"
+                    ></b-form-input>
+                  </b-form-group>
+                </b-col>
+                <b-col>
+                  <b-form-group
+                    id="input-group-correo"
+                    label="Correo electrónico:"
+                    label-for="input-correo"
+                    class="fontLabel"
+                  >
+                    <b-form-input
+                      id="input-correo"
+                      v-model="form.email"
+                      type="email"
+                      placeholder="Escribir correo electrónico"
+                      required
+                      class="fontInput"
+                    ></b-form-input>
+                  </b-form-group>
+                </b-col>
+              </b-row>
+
+              <b-row>
+                <b-col>
+                  <b-form-group
+                    @submit.stop.prevent
+                    id="input-group-contrasiena"
+                    label="Contraseña:"
+                    label-for="text-password"
+                    class="fontLabel"
+                  >
+                    <b-form-input
+                      type="password"
+                      id="text-password"
+                      aria-describedby="password-help-block"
+                      class="fontInput"
+                    ></b-form-input>
+                    <b-form-text id="password-help-block">
+                      Su contraseña debe tener entre 8 y 20.
+                    </b-form-text>
+                  </b-form-group>
+                </b-col>
+                <b-col>
+                  <b-form-group
+                    id="input-group-rol"
+                    label="Rol:"
+                    label-for="input-rol"
+                    class="fontLabel"
+                  >
+                    <b-form-select
+                      id="input-rol"
+                      v-model="form.rol"
+                      :options="rol"
+                      required
+                      class="fontInput"
+                    ></b-form-select>
+                  </b-form-group>
+                </b-col>
+              </b-row>
+              <b-row>
+                <b-col align="right">
+                  <b-button type="submit" variant="guardar" class="text-white">Guardar</b-button>
+                  <b-button href="#/usuarios" type="reset" variant="eliminar">Cancelar</b-button>
+                </b-col>
+              </b-row>
+            </b-container>
+          </b-form>
+          <b-card class="mt-3" header="Form Data Result">
+            <pre class="m-0">{{ form }}</pre>
+          </b-card>
+        </b-jumbotron>
+      </b-container>
+    </div>
+  </div>
 </template>
 
 <script>
-    
-
+export default {
+  name: "AgregarUsuario",
+  data() {
+    return {
+      users:[],
+      form: {
+        name: "",
+        email: "",
+        rol: null,
+      },
+      rol: [
+        { text: "Elige un rol", value: null },
+        "Investigador",
+        "Administrador",
+      ],
+      show: true,
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(event) {
+      event.preventDefault();
+      // Reset our form values
+      this.form.name = "";
+      this.form.email = "";
+      this.form.rol = null;
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = false;
+      });
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+
+.fontInput{
+  background-color: #F5F7FF;
+  color: #4283EB;
+}
+
+.fontLabel{
+  color: #767676;
+  font-weight: bold;
+}
+
+.separador{
+ border: 1px solid #767676;
+ border-radius: 50px;
+ background-color: gray;
+}
 
 </style>
--- src/views/MODULO_USUARIOS/ViewUsuarios.vue
+++ src/views/MODULO_USUARIOS/ViewUsuarios.vue
@@ -1,24 +1,172 @@
 <template>
-<div>
-
-    <br> <br>
-     <!-- Modificar a partir de aqui  -->
-    <b-container fluid class="mt--12">
-      <b-row> 
+  <div>
+    <br />
+    <br />
+    <b-container fluid>
+      <b-row>
         <b-col>
-          <h1> Módulo de Usuarios  </h1>
+          <h1>Usuarios</h1>
+        </b-col>
+        <b-col align="right">
+          <b-button href="#/agregarUsuario" variant="primario">
+            Agregar Usuario
+          </b-button>
         </b-col>
       </b-row>
-    </b-container>
 
+      <b-jumbotron
+        bg-variant="white"
+        class="shadow-lg p-5 mb-5 bg-white rounded"
+      >
+        <b-card
+          id="sombra"
+          bg-variant="white"
+          text-variant="black"
+          class="p-3 mb-5 bg-white rounded cardText"
+          v-for="user in users"
+          v-bind:key="user.idUsuario"
+        >
+          <b-container>
+            <b-card-title
+              ><h3>{{ user.nombreUsuario }}</h3>
+            </b-card-title>
+            <b-row align-v="center" class="card-item">
+              <b-col align="center" cols="12" md="12" lg="2" xl="2">
+                <span class="rounded-circle">
+                  <img
+                    v-bind="mainProps"
+                    alt="img apicultor"
+                    src="https://image.flaticon.com/icons/png/512/2658/2658085.png"
+                  />
+                </span>
+              </b-col>
+              <b-col cols="12" lg="4" xl="5">
+                <b-card-text class="cardText">
+                  <b-icon icon="person"></b-icon>
+                  {{ user.idUsuario }}
+                  <br />
+                  <b-icon icon="envelope"></b-icon>
+                  {{ user.correoElectronico }}
+                </b-card-text>
+              </b-col>
+              <b-col align-self="center" lg="3" xl="3">
+                <b-card-text>
+                  <b-icon icon="card-list"></b-icon>
+                  {{ user.rolUsuario.nombreRol }}
+                  <br />
+                  <b-icon icon="shield-lock"></b-icon> ***********
+                </b-card-text>
+              </b-col>
+              <b-col align-self="center" cols="12" lg="3" xl="2">
+                <b-button
+                  size="sm"
+                  block
+                  variant="modificar"
+                  @click="modificarUsuario(user.idUsuario)"
+                >
+                  <b-icon icon="pencil-fill"></b-icon> Modificar</b-button
+                >
+                <b-button
+                  id="popover-reactive-1"
+                  size="sm"
+                  block
+                  variant="eliminar">
+                  <b-icon icon="trash-fill"></b-icon>
+                  Eliminar
+                </b-button>
+                <b-popover
+                  target="popover-reactive-1"
+                  :show.sync="popoverShow"
+                >
+                  <template id="title_popover" #title>
+                    <b-button @click="onClose" class="close" aria-label="Close">
+                      <span class="d-inline-block" aria-hidden="true"
+                        >&times;</span
+                      >
+                    </b-button>
+                    Eliminar asociación
+                  </template>
+
+                  <div>
+                    <b-alert variant="" show class="small text-center">
+                      <strong>¿Desea eliminar la asociacion?</strong><br />
+                    </b-alert>
+                    <b-row align-h="center">
+                      <b-col cols="5">
+                        <b-button pill  size="sm" variant="primary"
+                          >Confirmar</b-button
+                        >
+                      </b-col>
+                    </b-row>
+                  </div>
+                </b-popover>
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-card>
+      </b-jumbotron>
+    </b-container>
   </div>
 </template>
 
 <script>
-
-
+import axios from "axios";
+export default {
+  name: "tarjeta",
+  data() {
+    return {
+      users: [],
+      popoverShow: false,
+      mainProps: {
+        blank: false,
+        blankColor: "#777",
+        width: 75,
+        height: 67,
+        class: "m1",
+      },
+    };
+  },
+  props: ["id"],
+  methods: {
+    obtenerUsuarios() {
+      const path = "http://localhost:9090/apimiel/web/usuarios";
+      axios
+        .get(path)
+        .then((response) => {
+          this.users = response.data;
+          console.log(this.users);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    modificarUsuario(id) {
+      this.$router.push(`/modificarUsuario/${id}`);
+    },
+    eliminarAsociacion(id) {
+      const path = `http://localhost:9090/apimiel/web/usuarios/eliminar/${id}`;
+      axios
+        .delete(path)
+        .then((response) => {
+          this.obtenerUsuarios();
+        })
+        .catch((error) => {
+          alert("ocurrio un error");
+        });
+    },
+    onClose() {
+      this.popoverShow = false;
+    },
+  },
+  created() {
+    this.obtenerUsuarios();
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+*#sombra {
+  box-shadow: 3px 5px 12px 3px gray;
+  border-radius: 10px, 10px, 10px, 10px;
+}
 </style>
